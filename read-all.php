bb9b6dc282e9fba830be5c899a10dfecaaf7f227
Primantoro Dwi Yogo <132739647+primantorodwiyogo@users.noreply.github.com>
2023-06-01 21:09:14 +0700

    Update read-all.php

--- read-all.php
+++ read-all.php
@@ -8,8 +8,6 @@ $sql = "SELECT * FROM todo";
 $result = mysqli_query($koneksi, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-  //mysqli_fetch_all gives us the data in 2D array format.
-  // It's second parameter decide whether its assoc array or indexed. Or maybe both
   $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
   echo json_encode($data);
